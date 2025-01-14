9bf192c01cee00eee1a4460f17a3662ec4f2e832
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-01-14 17:56:08 +0100

    auto generate compile commands

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,9 @@ add_requires("glfw")
 add_requires("glad", { configs = { api = "gl=3.3", profile = "core", spec = "gl"} })
 add_requires("imgui", { configs = { opengl3 = true, glfw = true } })
 
+-- update vscode intellisense
+add_rules("plugin.compile_commands.autoupdate", { outputdir = ".vscode" })
+
 -- project
 target("project")
 
